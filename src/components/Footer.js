2123b0d0a1898fa959ce55507c8d1924a81a3d2c
Nehaal <nshaikh2019@gmail.com>
2022-05-06 17:22:48 -0400

    updated files

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,22 +1,19 @@
-import React from 'react';
+import React from "react";
 import InstagramIcon from '@mui/icons-material/Instagram';
 import TwitterIcon from '@mui/icons-material/Twitter';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import'../styles/Footer.css';
+import "../styles/Footer.css";
 
 function Footer() {
   return (
-    <div className='footer'>
-        <div className='socialMedia'>
-            <InstagramIcon />
-            <TwitterIcon />
-            <FacebookIcon />
-            <LinkedInIcon />
-        </div>
-        <p> &copy; 2022 pizza.com</p>
+    <div className="footer">
+      <div className="socialMedia">
+        <InstagramIcon /> <TwitterIcon /> <FacebookIcon /> <LinkedInIcon />
+      </div>
+      <p> &copy; 2021 pedrotechpizza.com</p>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
--- src/pages/About.js
+++ src/pages/About.js
@@ -1,23 +1,33 @@
-import React from 'react';
-import MultiplePizzas from '../assets/multiplePizzas.jpeg';
-import '../styles/About.css'
-
+import React from "react";
+import MultiplePizzas from "../assets/multiplePizzas.jpeg";
+import "../styles/About.css";
 function About() {
   return (
-    <div className='about'>
-        <div className='aboutTop' style= {{ backgroundImage: `url(${MultiplePizzas })` }}></div>
-        <div className='aboutBottom'>
-            <h1>ABOUT US</h1>
-            <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-                labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit
-                esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
-                culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-        </div>
+    <div className="about">
+      <div
+        className="aboutTop"
+        style={{ backgroundImage: `url(${MultiplePizzas})` }}
+      ></div>
+      <div className="aboutBottom">
+        <h1> ABOUT US</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+          mollitia, molestiae quas vel sint commodi repudiandae consequuntur
+          voluptatum laborum numquam blanditiis harum quisquam eius sed odit
+          fugiat iusto fuga praesentium optio, eaque rerum! Provident similique
+          accusantium nemo autem. Veritatis obcaecati tenetur iure eius earum ut
+          molestias architecto voluptate aliquam nihil, eveniet aliquid culpa
+          officia aut! Impedit sit sunt quaerat, odit, tenetur error, harum
+          nesciunt ipsum debitis quas aliquid. Reprehenderit, quia. Quo neque
+          error repudiandae fuga? Ipsa laudantium molestias eos sapiente
+          officiis modi at sunt excepturi expedita sint? Sed quibusdam
+          recusandae alias error harum maxime adipisci amet laborum.
+          Perspiciatis minima nesciunt dolorem! Officiis iure rerum voluptates a
+          cumque velit
+        </p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default About
+export default About;
