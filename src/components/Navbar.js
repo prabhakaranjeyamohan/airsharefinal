7fa9a7ce20d4fa64ae23eb7576e302bae06b9c53
Prabhakaran Jeyamohan <prabha.jeyamohan@gmail.com>
2022-05-10 13:13:09 -0400

    Prabha commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,7 +13,7 @@ function Navbar() {
   return (
     <div className="navbar">
       <div className="leftSide" id={openLinks ? "open" : "close"}>
-        <img src={Logo} />
+        <img alt="logo" src={Logo} />
         <div className="hiddenLinks">
           <Link to="/"> Home </Link>
           <Link to="/menu"> Menu </Link>
